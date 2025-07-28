7e14ae69c7ee26e5379a083a42b9e3e482c8cc8b
Ludo <167148312+Sofia-VNZL@users.noreply.github.com>
2025-07-28 20:40:44 -0300

    Start
    Etapa 1, teste simples de validação

---
+++ Testes_fase_1/src/test/java/CanaryTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CanaryTest {
+
+    @Test
+    void func(){
+        var passou = true;
+        Assertions.assertTrue(passou);
+    }
+}
